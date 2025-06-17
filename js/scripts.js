9b65d2e7c234bc3006aa5ee275e8b2ed37f55f4b
LuisPereira05 <luispereira.sl014@academico.ifsul.edu.br>
2025-06-16 21:31:40 -0300

    Iniciando proyecto

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var catDropdown = document.querySelector("#catDropdown");
+var catSubmenu = document.querySelector("#catSubmenu");
+
+catDropdown.onmouseover = function name(params) {
+    catSubmenu.style.display = "grid";
+}
+
+catDropdown.onmouseout = function name(params) {
+    catSubmenu.style.display = "none";
+}
+
+catSubmenu.onmouseover = function name(params) {
+    catSubmenu.style.display = "grid";
+}
+
+catSubmenu.onmouseout = function name(params) {
+    catSubmenu.style.display = "none";
+}
