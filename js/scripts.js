ccae41298cae2589366c4f1f866840125a753dd4
LuisPereira05 <luispereira.sl014@academico.ifsul.edu.br>
2025-06-16 22:49:01 -0300

    Menu

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,18 @@ catSubmenu.onmouseover = function name(params) {
 catSubmenu.onmouseout = function name(params) {
     catSubmenu.style.display = "none";
 }
+
+var menuOn = false;
+
+document.querySelector("#showMenu").onclick = function name(params) {
+    if(menuOn) {
+        document.querySelector(".menuArea").style.left = "-79px";
+        menuOn = false;
+        document.querySelector("#showMenu img").style.transform = "scaleX(1)";
+    } else {
+        document.querySelector(".menuArea").style.left = 0;
+        menuOn = true;
+        document.querySelector("#showMenu img").style.transform = "scaleX(-1)";
+    }
+    
+}
